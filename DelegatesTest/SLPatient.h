3ed4dab575fa93f6404c84c77caa828ee31137fd
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-11-10 21:34:30 -0500

    Create delegate

--- DelegatesTest/SLPatient.h
+++ DelegatesTest/SLPatient.h
@@ -11,6 +11,22 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SLPatient : NSObject
 
+@property (strong, nonatomic) NSString* name;
+@property (assign, nonatomic) NSString* temperature;
+
+- (BOOL) hawAreYou;
+- (void) takePill;
+- (void) makeShot;
+
 @end
 
 NS_ASSUME_NONNULL_END
+
+
+
+@protocol SLPatientDelegate <NSObject>
+
+- (void) patientFeelsBad:(SLPatient*_Nonnull) patient;
+- (void) patient:(SLPatient*_Nonnull) patient hasQuestion:(NSString*_Nonnull) question;
+
+@end
