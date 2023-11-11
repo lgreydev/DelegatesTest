1dfdb6edf989989d9e8d745ddce76ee7b6187195
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-11-10 21:34:09 -0500

    Create delegate

---
+++ DelegatesTest/SLPatient.h
@@ -0,0 +1,16 @@
+//
+//  SLPatient.h
+//  DelegatesTest
+//
+//  Created by Sergey Lukaschuk on 2023-11-10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLPatient : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DelegatesTest/SLPatient.m
@@ -0,0 +1,12 @@
+//
+//  SLPatient.m
+//  DelegatesTest
+//
+//  Created by Sergey Lukaschuk on 2023-11-10.
+//
+
+#import "SLPatient.h"
+
+@implementation SLPatient
+
+@end
